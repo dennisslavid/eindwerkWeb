aee1c45511de264fbf2cbea2ccca86b3bc2d8b3e
Dennis Slavid <dennis.slavid@tvh.com>
2018-06-08 10:52:29 +0200

    Routing to landingPage ok

---
+++ src/java/Beans/InternshipEJB.java
@@ -0,0 +1,26 @@
+package Beans;
+
+import DAL.Internship;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author denni
+ */
+@Stateless
+public class InternshipEJB {
+
+    @PersistenceContext(unitName = "internshipWebPU")
+    private EntityManager em;
+    
+    public List<Internship> getAllInternships() {
+        List<Internship> resultList;
+        Query query = em.createNamedQuery("Internship.findAll");
+        resultList = query.getResultList();
+        return resultList;
+    }
+}
--- web/landingPage.jsp
+++ web/landingPage.jsp
@@ -12,6 +12,6 @@
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Ik ben op de fucking landingsPage.jsp</h1>
     </body>
 </html>
